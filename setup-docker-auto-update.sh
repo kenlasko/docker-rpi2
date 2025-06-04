089827398c7943ab163bf929857bf70ed4af5c21
Ken Lasko <ken.lasko@gmail.com>
2025-06-04 17:13:44 -0400

    Update script with more error checking

--- setup-docker-auto-update.sh
+++ setup-docker-auto-update.sh
@@ -26,7 +26,10 @@ cd /docker
 
 # Fetch remote changes
 echo "Fetching remote changes..."
-git fetch origin
+if ! timeout 30s git fetch origin; then
+  echo "$(date): git fetch timed out or failed."
+  exit 1
+fi
 
 # Check if anything actually changed
 echo "Checking for changes..."
